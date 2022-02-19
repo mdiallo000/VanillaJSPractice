bfd04a17852463d5228bbc35e9f33192fe82995a
mdiallo98 <mamadou1237@gmail.com>
2022-02-19 17:35:28 -0500

    twelve commit

--- ChuckNorrisJokes/app.js
+++ ChuckNorrisJokes/app.js
@@ -2,6 +2,8 @@
 
 const api_url = 'https://api.chucknorris.io/jokes/random';
 
+const Zlatan_Api =
+  ' https://cors-anywhere.herokuapp.com/http://www.zlatanjokes.site/joke ';
 // * Next, get the nodes to our button and the P element where we will display our jokes
 
 const btn = document.querySelector('.btn');
@@ -10,32 +12,15 @@ const content = document.querySelector('.content');
 // * now we can take our button and add a click event where whenever it is pressed we will make an api call and display random jokes.
 
 btn.addEventListener('click', () => {
-  getData(api_url);
+  fetch(api_url)
+    .then((response) => response.json())
+    .then((data) => (content.textContent = data.value))
+    .catch((err) => console.log(err));
 });
 
-// * We make a seperate function with our xhr object and send the request to the api server
+// * this function is responsible for displaying the data we get back from the api
 
-function getData(url) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.send();
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState != 4) return;
-    if (xhr.status === 200) {
-      console.log(xhr.responseText);
-      //* if we get the proper response we can then convert it into JSon so that we can extract the value out, because our intial response is in a string format
-      const response = JSON.parse(xhr.responseText);
-
-      //* Once we get our response object we can destruct the "value" property and give it an alias of our liking or we can use dot notation on the response variable.
-      content.textContent = response.value;
-    }
-
-    // ** this final condition will catch anything else and console log it
-    else {
-      console.log({
-        status: xhr.status,
-        text: xhr.statusText,
-      });
-    }
-  };
-}
+// function displayData(data) {
+//   const result = data;
+//   content.textContent = result.value;
+// }
---
+++ ChuckNorrisJokes/xhrVersion.js
@@ -0,0 +1,41 @@
+// *First get the url endpoint link where we will fetch our jokes
+
+const api_url = 'https://api.chucknorris.io/jokes/random';
+
+// * Next, get the nodes to our button and the P element where we will display our jokes
+
+const btn = document.querySelector('.btn');
+const content = document.querySelector('.content');
+
+// * now we can take our button and add a click event where whenever it is pressed we will make an api call and display random jokes.
+
+btn.addEventListener('click', () => {
+  getData(api_url);
+});
+
+// * We make a seperate function with our xhr object and send the request to the api server
+
+function getData(url) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.send();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState != 4) return;
+    if (xhr.status === 200) {
+      console.log(xhr.responseText);
+      //* if we get the proper response we can then convert it into JSon so that we can extract the value out, because our intial response is in a string format
+      const response = JSON.parse(xhr.responseText);
+
+      //* Once we get our response object we can destruct the "value" property and give it an alias of our liking or we can use dot notation on the response variable.
+      content.textContent = response.value;
+    }
+
+    // ** this final condition will catch anything else and console log it
+    else {
+      console.log({
+        status: xhr.status,
+        text: xhr.statusText,
+      });
+    }
+  };
+}
