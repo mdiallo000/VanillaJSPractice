fcb6bea0bdb8a73e9900c53118cd9acd881feae9
mdiallo98 <mamadou1237@gmail.com>
2022-02-20 05:15:52 -0500

    17 commit

--- PaginationPractice/fetchFollowers.js
+++ PaginationPractice/fetchFollowers.js
@@ -1,6 +1,6 @@
 const url = 'https://api.github.com/users/john-smilga/followers?per_page=100';
 
-const fetchFollowers = async (url) => {
+const fetchFollowers = async () => {
   const response = await fetch(url);
   const data = await response.json();
   return data;
