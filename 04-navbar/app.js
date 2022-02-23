dda29bd22eb33fb0ed444f1cc72bc405a46c5d29
mdiallo98 <mamadou1237@gmail.com>
2022-02-22 19:50:25 -0500

    25 commit

--- 04-navbar/app.js
+++ 04-navbar/app.js
@@ -3,3 +3,10 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const btn = document.querySelector('.nav-toggle');
+
+btn.addEventListener('click' (e)=>{
+        let Styles = e.target.classList
+
+})
